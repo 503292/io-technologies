d5d8360af17cdf25b394ce72e2b70c35718c5d6d
kalysh_maxym <503292@ukr.net>
2020-01-24 21:26:15 +0200

    Updates

---
+++ precache-manifest.f9232ffab9ffdef630143e506cff8d6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42400f021256048f11537ef7d7e2c511",
+    "url": "/io-technologies/index.html"
+  },
+  {
+    "revision": "3d73c34570fe2d0b4bc3",
+    "url": "/io-technologies/static/css/main.5367d2ef.chunk.css"
+  },
+  {
+    "revision": "8de69f4618a34753b628",
+    "url": "/io-technologies/static/js/2.88dbf78e.chunk.js"
+  },
+  {
+    "revision": "3d73c34570fe2d0b4bc3",
+    "url": "/io-technologies/static/js/main.ad345d02.chunk.js"
+  },
+  {
+    "revision": "ae4820c5bc81c707d845",
+    "url": "/io-technologies/static/js/runtime-main.3b56663f.js"
+  }
+]);
